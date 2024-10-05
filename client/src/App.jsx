c41eed44d059082ba9c86aa1d905268e68411300
Ayush Kashyap <ayushprokashyap@gmail.com>
2024-10-05 12:21:36 +0530

    added routinh

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import News from "./pages/News";
+import Analysis from "./pages/Analysis"; // Ensure Analysis component is imported
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/analysis" element={<Analysis />} />
+      </Routes>
+    </Router>
   );
 }
+
 export default App;
