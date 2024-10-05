7408f466afb8b4865258a23d94c2b152fd0bb95a
Ayush Kashyap <ayushprokashyap@gmail.com>
2024-10-05 21:57:32 +0530

    updated home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,9 +2,15 @@ import Background from "../components/Background";
 
 function Home() {
   return (
-    <div className="h-screen w-screen bg-[#1E1E1E]">
-      <Background />
+    <div className="bg-[#121212] overflow-hidden">
+      <div>
+        <Background />
+      </div>
+      <div className="flex justify-center items-center">
+        <div className="h-24 w-11/12 mt-3 ml-9 mr-9 bg-[#D9D9D9]/50 rounded-full"></div>
+      </div>
     </div>
   );
 }
+
 export default Home;
